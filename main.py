30de3bbf4acd6bfacf901db224f074196b6f1584
Andrew Ostrosky <frog7227@gmail.com>
2021-03-24 22:04:09 -0400

    Added median filtering to the binary image to reduce noise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py for the license plate reader
 import cv2
 import sys
+import numpy as np
 
 # takes in the first argument as the input file name
 if len(sys.argv) == 1:  # if only one argument was specified, the image file wasn't specified
@@ -15,6 +16,18 @@ grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 (_, binImg) = cv2.threshold(grayImg, 127, 255, cv2.THRESH_BINARY)
 # The underscore is where the threshold goes, but we don't care about it, so trash it
 
+# erode and dilate image to remove noise
+# se = np.ones((10, 10), np.uint8)
+
+blurred = cv2.medianBlur(binImg, 1)  # smooth out the image
+
+#se = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
+#eroded = cv2.erode(blurred, se, iterations=1)  # erode and dilate to reduce noise
+#dilated = cv2.dilate(eroded, se, iterations=1)
+
 cv2.imshow('Original image', img)
 cv2.imshow('Binary image', binImg)
+cv2.imshow('blurred', blurred)
+
+
 cv2.waitKey(0)  # pause so that the user can see the images
