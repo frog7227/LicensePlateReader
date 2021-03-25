088ba9a415452a831bb30f229ad29ce211911293
Andrew Ostrosky <frog7227@gmail.com>
2021-03-24 21:33:52 -0400

    Added image loading code, and file checking code, currently it just shows the original image and the binary image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py for the license plate reader
 import cv2
 import sys
+
 # takes in the first argument as the input file name
 if len(sys.argv) == 1:  # if only one argument was specified, the image file wasn't specified
     print("No image was specified")
@@ -10,6 +11,10 @@ img = cv2.imread(sys.argv[1])
 if img is None:  # if a file name was given, but it was invalid
     print("No valid image was specified")
     quit(1)
+grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+(_, binImg) = cv2.threshold(grayImg, 127, 255, cv2.THRESH_BINARY)
+# The underscore is where the threshold goes, but we don't care about it, so trash it
 
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
+cv2.imshow('Original image', img)
+cv2.imshow('Binary image', binImg)
+cv2.waitKey(0)  # pause so that the user can see the images
