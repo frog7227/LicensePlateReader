7da5dc7eca5341194896dd832497b08ef132cc5b
Andrew Ostrosky <frog7227@gmail.com>
2021-04-02 21:11:02 -0400

    Added resizing of image as well as restricted the char set for pytessarect

--- main.py
+++ main.py
@@ -13,24 +13,24 @@ img = cv2.imread(sys.argv[1])
 if img is None:  # if a file name was given, but it was invalid
     print("No valid image was specified")
     quit(1)
+lpRes = (600, 300)  # aspect ratio * res scale
+img = resized = cv2.resize(img, lpRes, interpolation=cv2.INTER_AREA)
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 (_, binImg) = cv2.threshold(grayImg, 127, 255, cv2.THRESH_BINARY)
 # The underscore is where the threshold goes, but we don't care about it, so trash it
 
-# erode and dilate image to remove noise
-# se = np.ones((10, 10), np.uint8)
+cv2.imwrite("binImg.jpg", binImg);
+blurred = cv2.GaussianBlur(binImg, (3, 3), 0)  # smooth out the image
 
-blurred = cv2.medianBlur(binImg, 1)  # smooth out the image
-
-#se = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
-#eroded = cv2.erode(blurred, se, iterations=1)  # erode and dilate to reduce noise
-#dilated = cv2.dilate(eroded, se, iterations=1)
-text = pytesseract.image_to_string(img)
+# se = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
+# eroded = cv2.erode(blurred, se, iterations=1)  # erode and dilate to reduce noise
+# dilated = cv2.dilate(eroded, se, iterations=1)
+text = pytesseract.image_to_string(binImg, lang='eng',config='--oem 3 --psm 6 -c '
+                                                    'tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!')
 print(text)
 
 cv2.imshow('Original image', img)
 cv2.imshow('Binary image', binImg)
 cv2.imshow('blurred', blurred)
-
 
 cv2.waitKey(0)  # pause so that the user can see the images
