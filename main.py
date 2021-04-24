d702865b293b3792e9297e53cbba37abc4582f75
Andrew Ostrosky <frog7227@gmail.com>
2021-04-24 14:31:31 -0400

    Added resizing of image as well as restricted the char set for pytessarect, as well as the state list

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import cv2
 import sys
 import numpy as np
 import pytesseract
+import difflib
+US_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut",
+             "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho",
+             "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine",
+             "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota",
+             "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma",
+             "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
+             "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia",
+             "Wyoming"]  # This list of states is used to find the closest matching state name from the license plate
 
 # takes in the first argument as the input file name
 if len(sys.argv) == 1:  # if only one argument was specified, the image file wasn't specified
@@ -28,6 +37,11 @@ blurred = cv2.GaussianBlur(binImg, (3, 3), 0)  # smooth out the image
 text = pytesseract.image_to_string(binImg, lang='eng',config='--oem 3 --psm 6 -c '
                                                     'tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!')
 print(text)
+print("\n\n\n")
+state = "PENNSYLVANIA"
+print("The State is: " + state)
+lpNumber = "HVY5774"
+print("The license plate number is: "+ lpNumber)
 
 cv2.imshow('Original image', img)
 cv2.imshow('Binary image', binImg)
